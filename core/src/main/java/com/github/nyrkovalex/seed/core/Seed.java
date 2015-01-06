dfb3b068d0251b7b386e1de4ee50dfa4b5ae97b6
Alexander Nyrkov <nyrkovalex@gmail.com>
2015-01-07 01:00:31 +0300

    Strings class

--- core/src/main/java/com/github/nyrkovalex/seed/core/Seed.java
+++ core/src/main/java/com/github/nyrkovalex/seed/core/Seed.java
@@ -155,4 +155,29 @@ public final class Seed {
             }
         }
     }
+
+    public static final class Strings {
+        private Strings() {
+        }
+
+        /**
+         * Joins items with provided separator.
+         *
+         * @param separator separator symbol
+         * @param items     items to join
+         * @return string representation of items joined with separator
+         */
+        public static String join(String separator, Iterable<?> items) {
+            StringBuilder builder = new StringBuilder();
+            boolean appendSeparator = false;
+            for (Object item : items) {
+                if (appendSeparator) {
+                    builder.append(separator);
+                }
+                builder.append(item);
+                appendSeparator = true;
+            }
+            return builder.toString();
+        }
+    }
 }
