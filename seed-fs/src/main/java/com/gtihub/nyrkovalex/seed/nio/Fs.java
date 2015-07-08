4c013963fb4190dda1193656bf52dca63f8d14a0
Alexander Nyrkov <nyrkovalex@gmail.com>
2015-07-09 00:15:20 +0300

    Fs.deleteWithContents() fixed

--- seed-fs/src/main/java/com/gtihub/nyrkovalex/seed/nio/Fs.java
+++ seed-fs/src/main/java/com/gtihub/nyrkovalex/seed/nio/Fs.java
@@ -14,19 +14,19 @@ import java.util.Iterator;
 
 public class Fs {
 	private static final Fs INSTANCE = new Fs();
-	
+
 	private Fs() {
-		
+
 	}
-	
+
 	public Path path(String first, String... more) {
 		return Paths.get(first, more);
 	}
-	
+
 	public Path tempDir(String prefix, FileAttribute<?>... attrs) throws IOException {
 		return Files.createTempDirectory(prefix, attrs);
 	}
-	
+
 	public Path tempFile(String prefix, String suffix, FileAttribute<?> attrs) throws IOException {
 		return Files.createTempFile(prefix, suffix, attrs);
 	}
@@ -34,36 +34,40 @@ public class Fs {
 	public Path copy(Path source, Path target, CopyOption... options) throws IOException {
 		return Files.copy(source, target, options);
 	}
-	
+
 	public void delete(Path path) throws IOException {
 		Files.delete(path);
 	}
-	
+
 	public void deleteWithContents(Path path) throws IOException {
-		if (!Files.isDirectory(path)) {
-			delete(path);
-			return;
-		}
 		Iterator<Path> iter = Files.list(path).iterator();
-		while(iter.hasNext()) {
+		while (iter.hasNext()) {
 			Path next = iter.next();
-			deleteWithContents(next);
-			delete(next);
+			if (isDirectory(next)) {
+				deleteWithContents(next);
+			} else {
+				delete(next);
+			}
 		}
+		delete(path);
 	}
-	
+
+	public boolean isDirectory(Path path, LinkOption... options) {
+		return Files.isDirectory(path, options);
+	}
+
 	public Path write(Path path, Iterable<? extends CharSequence> lines, OpenOption... options) throws IOException {
 		return Files.write(path, lines, options);
 	}
-	
+
 	public BufferedWriter newBufferedWriter(Path path, OpenOption... options) throws IOException {
 		return Files.newBufferedWriter(path, options);
 	}
-	
+
 	public BufferedReader newBufferedReader(Path path) throws IOException {
 		return Files.newBufferedReader(path);
 	}
-	
+
 	public boolean deleteIfExists(Path path) throws IOException {
 		return Files.deleteIfExists(path);
 	}
@@ -71,7 +75,7 @@ public class Fs {
 	public boolean exists(Path path, LinkOption... options) {
 		return Files.exists(path, options);
 	}
-	
+
 	public static Fs instance() {
 		return INSTANCE;
 	}
